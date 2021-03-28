3c8b132e14b2ca23d19d3955a194b65bf635c209
Kioshi Okamoto <kjor29@gmail.com>
2021-03-28 14:32:46 -0500

    :fire: NEW: creacion de proyecto y scripts generales en package.json

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
