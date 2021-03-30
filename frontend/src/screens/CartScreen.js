34d322873a6c2d246a472e48c13ab08f8745609e
Kioshi Okamoto <kjor29@gmail.com>
2021-03-29 23:05:26 -0500

    :dart: IMPROVE: creacion de redux, declaracion de constants y avance en reducers

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -2,23 +2,27 @@ import CartItem from '../components/CartItem';
 import './CartScreen.css';
 
 const CartScreen = () => {
-    return (
-        <div className="cartscreen">
-            <div className="cartscreen__left">
-                <h2>Shopping Cart</h2>
-                <CartItem />
-            </div>
-            <div className="cartscreen__right">
-                <div className="cartscreen__info">
-                    <p>Subtotal (0) items</p>
-                    <p>$499.99</p>
-                </div>
-                <div>
-                    <button>Proceed to checkout</button>
-                </div>
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className="cartscreen">
+			<div className="cartscreen__left">
+				<h2>Shopping Cart</h2>
+				<CartItem />
+				<CartItem />
+				<CartItem />
+				<CartItem />
+			</div>
+			<div className="cartscreen__right">
+				<div className="cartscreen__info">
+					<p>Subtotal (0) items</p>
+					<p>$499.99</p>
+				</div>
 
-export default CartScreen
+				<div>
+					<button>Proceed to checkout</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default CartScreen;
